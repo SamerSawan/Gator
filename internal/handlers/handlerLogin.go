679ce10af84cb8e9ef6cab1ec00cc56dbcfc34a8
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-11-16 22:47:25 -0500

    added a check to the login command to verify that user exists before logging in

--- internal/handlers/handlerLogin.go
+++ internal/handlers/handlerLogin.go
@@ -1,15 +1,22 @@
 package handlers
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"os"
 )
 
 func HandlerLogin(s *State, cmd Command) error {
 	if len(cmd.Args) != 1 {
 		return errors.New("You must enter a username!")
 	}
-	err := s.Cfg.SetUser(cmd.Args[0])
+	_, err := s.Db.GetUser(context.Background(), cmd.Args[0])
+	if err != nil {
+		fmt.Println("User does not exist!")
+		os.Exit(1)
+	}
+	err = s.Cfg.SetUser(cmd.Args[0])
 	if err != nil {
 		return err
 	}
